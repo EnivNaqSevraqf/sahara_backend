2c77c310eb8b19576ff37230583c22a3f5c37cc1
rcharan05 <152679442+rcharan05@users.noreply.github.com>
2025-03-21 16:26:18 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, JSON
 from database import Base
 
 class Announcements(Base):
     __tablename__ = 'announcements'
     id=Column(Integer, primary_key=True, index=True)
     title = Column(String)
-    description = Column(String)
+    description = Column(JSON, nullable=True)
+    file_path=Column(String, nullable=True)
